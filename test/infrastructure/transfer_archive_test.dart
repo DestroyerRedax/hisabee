74c88c778e695508d53178a32305962e53c031c9
DestroyerRedax <destroyerredax@gmail.com>
2026-07-23 12:03:15 +0600

    Add error reason to restoredProfs.isSuccess assertion

--- test/infrastructure/transfer_archive_test.dart
+++ test/infrastructure/transfer_archive_test.dart
@@ -123,7 +123,7 @@ void main() {
       // 5. Verify restored records
       final restoredProfs = await profileRepo.getActiveProfiles();
       final restoredTx = await txRepo.getActiveTransactionsForProfile('prof_round');
-      expect(restoredProfs.isSuccess, isTrue);
+      expect(restoredProfs.isSuccess, isTrue, reason: restoredProfs.errorMessageOrNull ?? restoredProfs.toString());
       expect(restoredProfs.dataOrNull?.length, equals(1), reason: 'restoredProfs actual length is ${restoredProfs.dataOrNull?.length}, values: ${restoredProfs.dataOrNull?.map((p) => '${p.id}:${p.name}').toList()}');
       expect(restoredTx.isSuccess, isTrue, reason: restoredTx.errorMessageOrNull ?? restoredTx.toString());
       expect(restoredTx.dataOrNull?.length, equals(1), reason: 'Import stats: accepted=${stats.acceptedCount}, dup=${stats.duplicateCount}, rej=${stats.rejectedCount}');
